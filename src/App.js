32337e6869248daff6f74567eb1751565f1d878f
U Sen Wan <senwan96@gmail.com>
2022-03-18 21:34:21 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Menubar from './components/Menubar/Menubar';
 
 function App() {
   return (
     <div className="App">
-      <Menubar></Menubar>
-      <h2 className='text-primary'>Hello Fake Shop</h2>
+      
     </div>
   );
 }
--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Menubar = () => {
     return (
         <div>
-            <h1>Hello From Menubar</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-2">logo</div>
+                    <div className="col-md-10"></div>
+                </div>
+            </div>
         </div>
     );
 };
