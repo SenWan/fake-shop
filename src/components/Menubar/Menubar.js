ac43b60f9f660f4213e9382921180074a06c3c8a
U Sen Wan <senwan96@gmail.com>
2022-05-11 00:21:36 +0600

    initial commit

--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -5,7 +5,7 @@ const Menubar = (props) => {
         <div>
             <div className="container">
                 <div className="row">
-                    <div className="col-md-2 p-2 text-primary fw-bold "><h1>FAKESHOPBD</h1></div>
+                    <div className="col-md-2 p-2 text-primary fw-bold "><h1>FAKESHOP</h1></div>
                     <div className="col-md-10 menu-container d-flex justify-content-end fw-bold">
                         <li className='items me-4 p-2'>Home</li>
                         <li className='items me-4 p-2'>Contact</li>
